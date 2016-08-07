e8e120402f14badb89f84016bf0f40885fef2225
Daniel Hodges <hodges.daniel.scott@gmail.com>
2016-08-07 17:07:51 -0400

    initial release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name             = "mmal",
     description      = "Meteorological Middleware Application Layer",
     url              = "https://github.com/hodgesds/mmal-python",
-    version          = "0.0.0",
+    version          = "0.0.1",
     author           = "Daniel Hodges",
     author_email     = "hodges.daniel.scott@gmail.com",
     scripts          = [ "bin/mmal" ],
