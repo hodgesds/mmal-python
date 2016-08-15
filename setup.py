6e299a4ce435bd0d246b92d1e95da8870f215a2f
Daniel Hodges <hodges.daniel.scott@gmail.com>
2016-08-14 20:55:40 -0400

    sync with pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name             = "mmal",
     description      = "Meteorological Middleware Application Layer",
     url              = "https://github.com/hodgesds/mmal-python",
-    version          = "0.0.2",
+    version          = "0.0.3",
     author           = "Daniel Hodges",
     author_email     = "hodges.daniel.scott@gmail.com",
     scripts          = [ "bin/mmal" ],
